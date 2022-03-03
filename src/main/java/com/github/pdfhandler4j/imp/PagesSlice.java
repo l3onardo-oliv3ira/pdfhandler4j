85ee7213de1f70d4420a88bfecf7a0d9dee59ff7
Leonardo de Lima Oliveira <l3onardo-oliv3ira@gmail.com>
2022-03-03 13:11:06 -0400

    refactoring and improvments

--- src/main/java/com/github/pdfhandler4j/imp/PagesSlice.java
+++ src/main/java/com/github/pdfhandler4j/imp/PagesSlice.java
@@ -1,9 +1,9 @@
 package com.github.pdfhandler4j.imp;
 
-import com.github.filehandler4j.imp.FileSlice;
+import com.github.filehandler4j.imp.DefaultFileSlice;
 import com.github.pdfhandler4j.IPagesSlice;
 
-public final class PagesSlice extends FileSlice implements IPagesSlice {
+public final class PagesSlice extends DefaultFileSlice implements IPagesSlice {
 
   public PagesSlice() {
     this(1, Integer.MAX_VALUE);
